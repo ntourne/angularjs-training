82f207a336296d0ed1a65577b3abb58f38fac904
Nicolas Tourne <ntourne@beerealit.com>
2014-03-15 18:57:24 -0300

    filters

---
+++ 03_filters/filters/main.js
@@ -0,0 +1,20 @@
+var myApp = angular.module('myApp', []);
+
+myApp.factory('Data', function() {
+    return { message: "I'm data from a service"}
+});
+
+// create a filter
+myApp.filter('reverse', function() {
+    return function(text) {
+        return text.split("").reverse().join("");
+    }
+})
+
+function FirstCtrl($scope, Data) {
+    $scope.data = Data;
+}
+
+function SecondCtrl($scope, Data) {
+    $scope.data = Data;
+}
